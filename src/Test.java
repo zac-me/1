af5cf86c989671890ebefaa9d5c5d06b4e6f73c9
zac-me <321005310@qq.com>
2025-05-28 19:25:17 +0800

    4

---
+++ src/Test.java
@@ -0,0 +1,15 @@
+public class Test {
+    public static void main(String[]args)throws Exception{
+        SubwayNetwork subway =new SubwayNetwork();
+        subway.loadData("src/subway.txt");
+        //System.out.println(subway.getStations());
+        //System.out.println(subway.getLines());
+        //System.out.println(subway.getGraph());
+        //System.out.println(subway.getTransferStations());
+        //System.out.println(subway.findShortestPath("七里庙", "华中科技大学"));
+        //System.out.println(subway.findNearbyStations("华中科技大学", 1.0));
+        //System.out.println(subway.findAllPaths("华中科技大学", "红霞"));
+        //subway.printJourneyGuide(subway.findShortestPath("华中科技大学", "红霞"));
+        //subway.selectPathAndCalculateFare("华中科技大学", "红霞");
+        }
+    }
