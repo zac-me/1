02fa9a0894e8a19cc6ba5ff772cbe595c3844ebb
zac-me <321005310@qq.com>
2025-05-29 15:44:42 +0800

    5

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,7 @@
 public class Test {
     public static void main(String[]args)throws Exception{
         SubwayNetwork subway =new SubwayNetwork();
-        subway.loadData("src/subway.txt");
+        subway.loadData("1/src/subway.txt");
         //System.out.println(subway.getStations());
         //System.out.println(subway.getLines());
         //System.out.println(subway.getGraph());
